72ccb358cf6c3a109799b4a0cfc8e509eea51f66
flashguy22 <gw@rockbox.de>
2018-05-23 20:53:16 +0000

    swiping, timer, loading, display

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,6 +11,8 @@ import {
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
+import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
+
 var Buffer = require('buffer/').Buffer;
 
 export default class HomeScreen extends React.Component {
@@ -18,6 +20,9 @@ export default class HomeScreen extends React.Component {
     header: null,
   };
 
+  isInit = true
+
+  reloadDuration = 10000;
   tweelingsCollection = {};
   currentTweelingIndex = 0;
   currentTweeling = {};
@@ -25,25 +30,110 @@ export default class HomeScreen extends React.Component {
   tweelingCategories = [{ tweeling: "love", colour: { gradient1: 0x662483, gradient2: 0xC188BB, gradient3: 0x662483, textHighlight: "0x6c2356" }, standard: true }, { tweeling: "think", colour: { gradient1: 0x294697, gradient2: 0x009FE3, gradient3: 0x294697, textHighlight: "0x193890" }, standard: true }, { tweeling: "believe", colour: { gradient1: 0x006633, gradient2: 0x3AAA35, gradient3: 0x006633, textHighlight: "0x0E3515" }, standard: true }, { tweeling: "feel", colour: { gradient1: 0xE94E1B, gradient2: 0xF9B233, gradient3: 0xE94E1B, textHighlight: "0x781f00" }, standard: true }, { tweeling: "wish", colour: { gradient1: 0xDEB869, gradient2: 0xFFEBA6, gradient3: 0xDEB869, textHighlight: "0xB58A3E" }, standard: true }];
   tweelingToDisplay = "";
 
+  colors0 = { gradient1: 0x7089ce, gradient2: 0x7ae39a, gradient3: 0x7089ce, textHighlight: "0x5b80e4" };
+  colors1 = { gradient1: 0xffd599, gradient2: 0x7ae39a, gradient3: 0xffd599, textHighlight: "0x508c5f" };
+  colors2 = { gradient1: 0x9d967a, gradient2: 0x2e4036, gradient3: 0x9d967a, textHighlight: "0x18211b" };
+  colors3 = { gradient1: 0xe4bb17, gradient2: 0x4c2075, gradient3: 0xe4bb17, textHighlight: "0xd7b560" };
+  colors4 = { gradient1: 0xd317e4, gradient2: 0x4b7cec, gradient3: 0xd317e4, textHighlight: "0xffe16a" };
+  colors5 = { gradient1: 0x17e4d8, gradient2: 0xf4fb8c, gradient3: 0x17e4d8, textHighlight: "0x0e7994" };
+  colors6 = { gradient1: 0xe41717, gradient2: 0x8f0bf5, gradient3: 0xe41717, textHighlight: "0x0a0647" };
+  colors7 = { gradient1: 0xf0ff00, gradient2: 0x2a92bd, gradient3: 0xf0ff00, textHighlight: "0x493a12" };
+  colors8 = { gradient1: 0x00f8f5, gradient2: 0xbfbfbf, gradient3: 0x00f8f5, textHighlight: "0x7a84eb" };
+  colors9 = { gradient1: 0x401c0f, gradient2: 0xcc7373, gradient3: 0x401c0f, textHighlight: "0xde5323" };
+  colors10 = { gradient1: 0x00eca1, gradient2: 0xcc9f73, gradient3: 0x00eca1, textHighlight: "0x256b54" };
+  colors11 = { gradient1: 0xbfd122, gradient2: 0x5e4fbd, gradient3: 0xbfd122, textHighlight: "0xffcc00" };
+  colors12 = { gradient1: 0x00ff00, gradient2: 0xff00ae, gradient3: 0x00ff00, textHighlight: "0xfffc00" };
+  colors13 = { gradient1: 0xeb73bc, gradient2: 0xe1ea79, gradient3: 0xeb73bc, textHighlight: "0xfffc00" };
+  colors14 = { gradient1: 0x08be22, gradient2: 0x6978c2, gradient3: 0xe08be22, textHighlight: "0xfffc00" };
+  colors15 = { gradient1: 0x08beb8, gradient2: 0xc2698f, gradient3: 0x08beb8, textHighlight: "0x6b2f25" };
+  colors16 = { gradient1: 0xf1ff0b, gradient2: 0x69c2b6, gradient3: 0xf1ff0b, textHighlight: "0x25296b" };
+  colors17 = { gradient1: 0xdc25d1, gradient2: 0x69c2b6, gradient3: 0xdc25d1, textHighlight: "0x25296b" };
+  colors18 = { gradient1: 0xdc25d1, gradient2: 0xead100, gradient3: 0xdc25d1, textHighlight: "0x9d2ac8" };
+  colors19 = { gradient1: 0xdc25d1, gradient2: 0xa0ea00, gradient3: 0xdc25d1, textHighlight: "0x2037ab" };
+  colors20 = { gradient1: 0xec0000, gradient2: 0x6976f2, gradient3: 0xec0000, textHighlight: "0x500c69" };
+
+  activeColors = {};
+
   constructor() {
     super()
     this.state = {
-      tweelingToShow: ''
+      tweelingToShow: '',
+      myText: 'I\'m ready to get swiped!',
+      gestureName: 'none',
+      backgroundColor: '#fff',
     }
   }
 
-  componentDidMount() {
+  onSwipeUp(gestureState) {
 
-    this._interval = setInterval(() => {
+    if (this.activeCategory < this.tweelingCategories.length - 1) {
+      this.activeCategory++;
+    } else {
+      this.activeCategory = 0;
+    }
+    loadTweeling(this.tweelingCategories[this.activeCategory]);
+
+    this.setState({ myText: 'You swiped up! Now showing ' + this.tweelingCategories[this.activeCategory].tweeling });
+  }
+
+  onSwipeDown(gestureState) {
+    if (this.activeCategory > 0) {
+      this.activeCategory--;
+    } else {
+      this.activeCategory = this.tweelingCategories.length - 1
+    }
+    loadTweeling(this.tweelingCategories[this.activeCategory]);
+    this.setState({ myText: 'You swiped down! Now showing ' + this.tweelingCategories[this.activeCategory].tweeling });
+  }
+
+  onSwipeLeft(gestureState) {
+    this.setState({ myText: 'You swiped left!' });
+    if (this.currentTweelingIndex <= this.tweelingsCollection.length) {
       this.currentTweelingIndex++;
       displayTweeling();
-    }, 7000);
+    }
+  }
 
+  onSwipeRight(gestureState) {
+    this.setState({ myText: 'You swiped right!' });
+    if (this.currentTweelingIndex > 0) {
+      this.currentTweelingIndex--;
+      displayTweeling();
+    }
+    
+  }
+
+
+
+  componentDidMount() {
+
+    this.activeColors = this.colors0;
     this.getBearer();
+  }
+
+  reloadTimer = (action) => {
+    if (action == "start") {
+      this._interval = setInterval(() => {
+        this.currentTweelingIndex++;
+        displayTweeling();
+      }, this.reloadDuration);
+    } else if (action == "stop") {
+      clearInterval(this._interval);
+    }
+
   }
 
   componentWillUnmount() {
     clearInterval(this._interval);
+  }
+
+  nextCategory = () => {
+    if (this.activeCategory > 0) {
+      this.activeCategory--;
+    } else {
+      this.activeCategory = this.tweelingCategories.length - 1
+    }
+    loadTweelings(this.tweelingCategories[this.activeCategory]);
   }
 
   getBearer = () => {
@@ -78,15 +168,29 @@ export default class HomeScreen extends React.Component {
     }).then((response) => response.json())
       .then((responseData) => {
         bearerToken = responseData.access_token;
-        console.log("Got Bearer: " + responseData.access_token);
-        loadTweet();
+        console.log("Got Bearer: "/* + responseData.access_token*/);
+        //loadTweeling();
+        loadTweeling(this.tweelingCategories[this.activeCategory]);
+
 
       });
 
-    loadTweet = () => {
-      console.log("start load tweet");
+    loadTweeling = (q) => {
 
-      fetch('https://api.twitter.com/1.1/search/tweets.json?q=' + this.tweelingCategories[this.activeCategory].tweeling, {
+      if (q != this.currentTweeling) {
+        this.isInit = true;
+        //tweeling.visible = false;
+        //btnAuthor.visible = false;
+        //tweelingArrayCollection = new ArrayCollection();
+        this.currentTweelingIndex = 0;
+        this.activeColors = q.colour;
+
+
+        //arangeBG();
+      }
+      this.currentTweeling = q;
+      console.log("loadTweelings: " + q.tweeling)
+      fetch('https://api.twitter.com/1.1/search/tweets.json?q=' + q.tweeling, {
         method: 'GET',
         headers: {
           'Authorization': "Bearer " + bearerToken
@@ -94,17 +198,32 @@ export default class HomeScreen extends React.Component {
       }).then((response) => response.json())
         .then((responseData) => {
           console.log("Tweets Loaded successfully...")
-          this.tweelingsCollection = responseData.statuses;
+
           //console.log(JSON.stringify(responseData.statuses));
-          displayTweeling();
+          this.tweelingsCollection = responseData.statuses;
+
+          if (this.isInit) {
+            this.isInit = false;
+            this.reloadTimer("start");
+            this.currentTweelingIndex++;
+            displayTweeling();
+          }
+
+
 
         });
     };
 
     displayTweeling = () => {
-      var data = this.tweelingsCollection;
-      var index = this.currentTweelingIndex;
-      var author = data[index].user.name;
+
+      console.log("currentindex:" + this.currentTweelingIndex + " length: " + this.tweelingsCollection.length)
+      if (this.currentTweelingIndex + 1 === this.tweelingsCollection.length) {
+        loadTweeling(this.currentTweeling);
+        this.currentTweelingIndex = 0;
+        return;
+      }
+
+      var author = this.tweelingsCollection[this.currentTweelingIndex].user.name;
       //console.log(data[index].user.name
 
       /*for(let i = 0; i < data.length; i++){
@@ -114,7 +233,7 @@ export default class HomeScreen extends React.Component {
       var myPattern = new RegExp(this.currentTweeling.tweeling, "gi");
       var theString = this.tweelingsCollection[this.currentTweelingIndex].text.toLocaleLowerCase();
       //trace("string: " + theString)
-      var newString = theString; //theString.replace(myPattern, "<span color='" + activeColors.textHighlight + "' fontWeight='bold' fontFamily='azoft'>" + currentTweeling.tweeling + "</span>");
+      var newString = theString;//theString.replace(myPattern, "<Text style={{color: '" + this.activeColors.textHighlight + "'}}>" + this.currentTweeling.tweeling + " </Text>");
       // pattern to replace <3
       myPattern = new RegExp("<3", "gi");
       newString = newString.replace(myPattern, "♥");
@@ -148,47 +267,79 @@ export default class HomeScreen extends React.Component {
       }
       console.log(newString);
 
-      this.setState({tweelingToShow: newString})
+      this.reloadTimer("stop");
+      this.reloadTimer("start");
 
-      
-
-
+      this.setState({ tweelingToShow: newString })
     };
+
+
   };
 
 
   render() {
+
+
+    const config = {
+      velocityThreshold: 0.3,
+      directionalOffsetThreshold: 80
+    };
+
     return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
+
+
+      <GestureRecognizer
+        onSwipeUp={(state) => this.onSwipeUp(state)}
+        onSwipeDown={(state) => this.onSwipeDown(state)}
+        onSwipeLeft={(state) => this.onSwipeLeft(state)}
+        onSwipeRight={(state) => this.onSwipeRight(state)}
+        config={config}
+        style={{
+          flex: 1,
+          backgroundColor: this.state.backgroundColor
+        }}
+      >
+        <View style={styles.container}>
+          <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+            <View style={styles.welcomeContainer}>
+              <Image
+                source={
+                  __DEV__
+                    ? require('../assets/images/robot-dev.png')
+                    : require('../assets/images/robot-prod.png')
+                }
+                style={styles.welcomeImage}
+              />
+            </View>
+
+            <View style={styles.getStartedContainer}>
+              {this._maybeRenderDevelopmentModeWarning()}
+
+
+
+              <Text style={styles.tweelingText}>
+                {this.state.tweelingToShow}
+              </Text>
+
+
+
+
+
+
+            </View>
+            <View style={{ alignItems: 'center', marginTop: 50 }}>
+              <Text>{this.state.myText}</Text>
+            </View>
+
+
+          </ScrollView>
+
+          <View style={styles.tabBarInfoContainer}>
+            <Text style={styles.tabBarInfoText}>Info Container</Text>
           </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-
-
-            <Text style={styles.getStartedText}>
-              Tweeling: {this.state.tweelingToShow}
-            </Text>
-          </View>
-
-
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>Info Container</Text>
         </View>
-      </View>
+
+      </GestureRecognizer>
     );
   }
 
@@ -272,6 +423,12 @@ const styles = StyleSheet.create({
     fontSize: 17,
     color: 'rgba(96,100,109, 1)',
     lineHeight: 24,
+    textAlign: 'center',
+  },
+  tweelingText: {
+    fontSize: 28,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 34,
     textAlign: 'center',
   },
   tabBarInfoContainer: {
