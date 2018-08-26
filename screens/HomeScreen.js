284c36086682cb5d3b510aec78c9b78892b90b2e
Gabor Wraight <gabor.wraight@gmail.com>
2018-08-26 19:51:01 +0000

    nice ui

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -57,7 +57,7 @@ export default class HomeScreen extends React.Component {
     super()
     this.state = {
       tweelingToShow: '',
-      tweeling:'',
+      tweeling: '',
       author: '',
       authorImageURL: 'http://',
       myText: 'I\'m ready to get swiped!',
@@ -69,55 +69,54 @@ export default class HomeScreen extends React.Component {
 
   onShare = async () => {
 
-    const { width, height } = Dimensions.get('window');
-    const options = {
-      format: 'jpg',
-      quality: 0.75,
-      result: 'file',
-      height,
-      width,
-    };
-    const uri = await Expo.takeSnapshotAsync(this.refs.mainScreen, options);
-
-
-    theMessage = "Found this on tweelin.gs";
+    theMessage = "";
 
     if (Platform.OS === "android") {
-      theMessage = "Found this on tweelin.gs: " + this.state.tweelingToShow;
+      const { status_roll } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      const { width, height } = Dimensions.get('window');
+      const options = {
+        format: 'jpg',
+        quality: 0.75,
+        result: 'file',
+        height,
+        width,
+      };
+      const uri = await Expo.takeSnapshotAsync(this.refs.mainScreen, options);
+
+      let saveResult = await CameraRoll.saveToCameraRoll(uri, 'photo');
+      this.setState({ modalVisible: true });
+    } else {
+      const { width, height } = Dimensions.get('window');
+      const options = {
+        format: 'jpg',
+        quality: 0.75,
+        result: 'file',
+        height,
+        width,
+      };
+      const uri = await Expo.takeSnapshotAsync(this.refs.mainScreen, options);
+
+      Share.share({
+        message: "",
+        title: "Tweelings",
+        url: uri,
+        subject: "Share tweeling"
+      }, {
+          // Android only:
+          dialogTitle: 'Share Tweeling',
+          // iOS only:
+          excludedActivityTypes: [
+
+          ]
+        })
+
     }
 
-    Share.share({
-      message: theMessage,
-      title: "Tweelings",
-      url: uri,
-      subject: "Share tweeling"
-    }, {
-        // Android only:
-        dialogTitle: 'Share Tweeling',
-        // iOS only:
-        excludedActivityTypes: [
 
-        ]
-      })
+
   }
 
-  onSave = async () => {
 
-    const { status_roll } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-
-    const { width, height } = Dimensions.get('window');
-    const options = {
-      format: 'jpg',
-      quality: 0.75,
-      result: 'file',
-      height,
-      width,
-    };
-    const uri = await Expo.takeSnapshotAsync(this.refs.mainScreen, options);
-
-    let saveResult = await CameraRoll.saveToCameraRoll(uri, 'photo');
-    this.setState({ modalVisible: true });
-  }
 
 
   onSwipeUp(gestureState) {
@@ -240,7 +239,7 @@ export default class HomeScreen extends React.Component {
 
   loadTweeling = (q) => {
 
-    this.setState({ backgroundColor: this.tweelingCategories[this.activeCategory].colour.gradient1, tweelingToShow: "", tweeling:"#"+q.tweeling });
+    this.setState({ backgroundColor: this.tweelingCategories[this.activeCategory].colour.gradient1, tweelingToShow: "", tweeling: "#" + q.tweeling });
 
     if (q != this.currentTweeling) {
       this.isInit = true;
@@ -365,10 +364,10 @@ export default class HomeScreen extends React.Component {
 
   patternStyle = function (options) {
     return {
-      tintColor:this.tweelingCategories[this.activeCategory].colour.textHighlight, 
-      flex:1, 
-      alignSelf: 'stretch', 
-      height: undefined, 
+      tintColor: this.tweelingCategories[this.activeCategory].colour.textHighlight,
+      flex: 1,
+      alignSelf: 'stretch',
+      height: undefined,
       width: undefined
     }
   }
@@ -381,97 +380,87 @@ export default class HomeScreen extends React.Component {
 
     return (
       <GestureRecognizer
-      onSwipeUp={(state) => this.onSwipeUp(state)}
-      onSwipeDown={(state) => this.onSwipeDown(state)}
-      onSwipeLeft={(state) => this.onSwipeLeft(state)}
-      onSwipeRight={(state) => this.onSwipeRight(state)}
-      config={config}
-      style={{
-        flex: 1
-      }}
-    >
-      <View style={styles.container} ref="mainScreen">  
-         <LinearGradient colors={this.gradientColors()}
-            style={{ position:'absolute', width:'100%', height: '100%' }} /> 
-         <Image style={this.patternStyle()}
-          source={require('../assets/images/patterns/pattern.png')}
-          resizeMode="stretch"
-        />
-        <View style={{ position:'absolute', top:100, left:0 }}>
+        onSwipeUp={(state) => this.onSwipeUp(state)}
+        onSwipeDown={(state) => this.onSwipeDown(state)}
+        onSwipeLeft={(state) => this.onSwipeLeft(state)}
+        onSwipeRight={(state) => this.onSwipeRight(state)}
+        config={config}
+        style={{
+          flex: 1
+        }}
+      >
+        <View style={styles.container} ref="mainScreen">
+          <LinearGradient colors={this.gradientColors()}
+            style={{ position: 'absolute', width: '100%', height: '100%' }} />
+          <Image style={this.patternStyle()}
+            source={require('../assets/images/patterns/pattern.png')}
+            resizeMode="stretch"
+          />
+          <View style={{ position: 'absolute', top: 100, left: 0 }}>
 
-          <KeepAwake />
+            <KeepAwake />
+            <MonoText style={this.textStyle()} >
+              {this.state.tweelingToShow}
+            </MonoText>
+          </View>
 
-          <MonoText style={this.textStyle()} >
-            {this.state.tweelingToShow}
-          </MonoText>
-
-          
-
-        </View>
-          
-        
           <View style={styles.authorView}>
-          <Mask shape={'circle'}>
-            <Image
-              style={{ width: 50, height: 50 }}
-              source={{ uri: this.state.authorImageURL }}
-            />
-          </Mask>
-          <View style={{ flex:1, alignItems:'flex-start', flexDirection:'column'}}>
+            <Mask shape={'circle'}>
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={{ uri: this.state.authorImageURL }}
+              />
+            </Mask>
+            <View style={{ flex: 1, alignItems: 'flex-start', flexDirection: 'column' }}>
               <MonoText style={styles.authorText}>
                 by {this.state.author}
-              </MonoText> 
+              </MonoText>
               <MonoText style={styles.authorText}>
                 {this.state.tweeling}
               </MonoText>
-          </View>
-          <View style={{flex:1, flexShrink:1, flexDirection:'row'}}>
-            <Button style={styles.shareButton}
-                onPress={this.onShare}
-                title="Share"
-                color="#841584"
-              />
-              <Button style={styles.shareButton}
-                onPress={this.onSave}
-                title="Save"
-                color="#841584"
-              />
+            </View>
+            <View>
+              <TouchableHighlight onPress={this.onShare} underlayColor="white">
+                <Image
+                  style={{ width: 30, height: 30, tintColor: 'white' }}
+                  source={require('../assets/images/icons/share.png')}
+                />
+              </TouchableHighlight>
             </View>
           </View>
 
-           <View style={{ flex: 1, position:'absolute', alignItems: 'center', justifyContent: 'center' }}>
-              <Modal
-                animationType="slide"
-                transparent={true}
-                visible={this.state.modalVisible}
-                onRequestClose={() => {
-                  alert('Modal has been closed.');
-                }}>
-                <View style={styles.modalBottom}>
-                  <View style={styles.modalBottomContent}>
-                    <Text>Tweeling saved to your camera roll!</Text>
-
-                    <TouchableHighlight
-                      onPress={() => {
-                        this.setState({ modalVisible: false });
-                      }}>
-                      <Text>Thanks!</Text>
-                    </TouchableHighlight>
-                  </View>
+          <View style={{ flex: 1, position: 'absolute', alignItems: 'center', justifyContent: 'center' }}>
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={this.state.modalVisible}
+              onRequestClose={() => {
+                alert('Modal has been closed.');
+              }}>
+              <View style={styles.modalBottom}>
+                <View style={styles.modalBottomContent}>
+                  <Text>Tweeling saved to your camera roll!</Text>
+                  <TouchableHighlight
+                    onPress={() => {
+                      this.setState({ modalVisible: false });
+                    }}>
+                    <Text>Thanks!</Text>
+                  </TouchableHighlight>
                 </View>
-              </Modal>
-            </View>
-      </View>
-      
-      </GestureRecognizer>      
+              </View>
+            </Modal>
+          </View>
+        </View>
+
+      </GestureRecognizer>
     );
-  }  
+  }
 }
 
 const styles = StyleSheet.create({
 
   container: {
-    backgroundColor:'#000000',
+    backgroundColor: '#000000',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
@@ -479,9 +468,9 @@ const styles = StyleSheet.create({
 
   },
   patternImage: {
-    position:'absolute',
+    position: 'absolute',
 
-},
+  },
 
 
   developmentModeText: {
@@ -501,20 +490,14 @@ const styles = StyleSheet.create({
   },
   authorView: {
     flex: 1,
-    left:20,
-    right:20,
+    left: 20,
+    right: 20,
     flexDirection: 'row',
+    alignItems:'center',
     position: 'absolute',
-    bottom: 50,
+    bottom: 30,
   },
   shareButton: {
-    ...Platform.select({
-      ios: {
-      },
-      android: {
-        display: 'none',
-      },
-    }),
     marginLeft: 30,
     marginRight: 30
   },
